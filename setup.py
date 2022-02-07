f79983490715ae091744aa9206c68f86612c3d14
Koosha Tahmasebipour <koosha@viafoura.com>
2022-02-07 08:59:37 -0500

    testing scrapy with a simple tutorial

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=external_packages,
-    package_dir={"": "digger"},
-    packages=setuptools.find_packages(where="digger"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
