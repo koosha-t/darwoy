4d1775f51209e571a09e88c8dfa4ccb6adbfc9b4
Koosha Tahmasebipour <koosha@viafoura.com>
2022-02-07 21:15:22 -0500

    useful comments added to the scrappy tutorials

--- src/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ src/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -7,6 +7,11 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
+        """
+            must return an iterable of Requests (you can return a list of requests or write a generator function)
+            which the Spider will begin to crawl from. Subsequent requests will be generated successively
+            from these initial requests.
+        """
         urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
@@ -15,6 +20,13 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        """
+            This method will be called to handle the response downloaded for each of the requests made.
+            The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
+
+            The parse() method usually parses the response, extracting the scraped data as dicts and
+            also finding new URLs to follow and creating new requests (Request) from them.
+        """
         page = response.url.split("/")[-2]
         filename = f'quotes-{page}.html'
         with open(filename, 'wb') as f:
